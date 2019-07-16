e4f335fc6a2ec928426f0e70920af8890b88f839
Juank <jcgomez042@gmail.com>
2019-07-16 14:28:16 -0500

    bug render view on / fixed

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  return res.status(200).send({ message: 'Express' });
 });
 
 router.post('/upload', function(req, res){
 	console.log("files", req.files)
     if(req.files.image !== undefined){ // `image` is the field name from your form
-        res.send("success"); // success
+        return res.status(200).send("success"); // success
     }else{
-        res.send("error, no file chosen");
+        return res.status(400).send({message: "error, no file chosen"});
     }
 });
 
