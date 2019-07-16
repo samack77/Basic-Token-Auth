7835aef59527817dcff5c7c1458816373d77f9b5
Juank <jcgomez042@gmail.com>
2019-07-16 08:01:23 -0500

    public folder and its files removed from project, I don't need it xD

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(multer({ 
   limits : { fileSize:20000000 },
   storage: multerS3({
